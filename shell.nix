918e84082d051da3d1ab06ecf8e2438b28844f89
Marek Rychly <marek.rychly@gmail.com>
2020-07-01 18:10:16 +0200

    Nix-shell switched from mkDerivation to mkShell.

--- shell.nix
+++ shell.nix
@@ -1,15 +1,13 @@
 let
 
-  name = "convert-charsets";
+  overlays = [ ];	# import ./overlays.nix;
 
 in
   # Running against custom version of nixpkgs or pkgs would be as simple as running `nix-shell --arg nixpkgs /absolute/path/to/nixpkgs`
   # See https://garbas.si/2015/reproducible-development-environments.html
-  { nixpkgs ? import <nixpkgs>, pkgs ? nixpkgs { } }:
+  { nixpkgs ? import <nixpkgs>, pkgs ? nixpkgs { inherit overlays; } }:
 
-pkgs.stdenv.mkDerivation rec {
-
-  inherit name;
+pkgs.mkShell rec {
 
   buildInputs = with pkgs; [
     (luaPackages.lua.withPackages(ps: with ps; [ ]))
